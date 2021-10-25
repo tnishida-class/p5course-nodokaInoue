86e92f2f1dc12bd8a04a2a90f369a9989ea5073c
nodokaInoue <inoue_nodoka13525@icloud.com>
2021-10-25 09:41:55 +0900

    Change section2-q1,2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,26 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+
+
+
+if((i+j)%2==0){
+fill(255);
+}
+if((i+j)%2==1){
+fill(150);
+}
+rect(size*i, size*j, size, size);
+
+if((i+j)%2==1 && j<3){
+  fill(255, 0, 0);
+}
+if((i+j)%2==1 && j>4){
+  fill(0);
+}
+ellipse(size*(i+1/2), size*(j+1/2), 20);
+
+
     }
   }
 }
