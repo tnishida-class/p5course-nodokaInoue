eafa7f06f476832445d8f94ea606991ef61093ca
nodokaInoue <inoue_nodoka13525@icloud.com>
2021-11-10 23:01:28 +0900

    change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -11,8 +11,14 @@ function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let px = random(0,400-(w + p * 2));
+  let py = random(0,400-(h + p * 2));
+  let num1 = Math.floor(Math.random() * 256),
+      num2 = Math.floor(Math.random() * 256),
+      num3 = Math.floor(Math.random() * 256),
+      c = "rgb(" + num1 + "," + num2 + "," + num3 + ")";
+  fill(c);
+  rect(px, py, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, px+p, py+h);
 }
